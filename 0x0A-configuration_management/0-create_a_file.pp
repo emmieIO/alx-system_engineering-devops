c6b5bdd5047e9fe71cffefbd646f996d86f2e6ba
emmieIO <markonuoha6@gmail.com>
2024-01-22 13:46:46 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
-file { '/tmp/school':
-  ensure  => 'file',
+# Create a file in /tmp
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/school',
 }
