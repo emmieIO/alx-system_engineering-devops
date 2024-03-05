55ed2f504672d93057c2bf771d65b521060fdbfa
emmieIO <markonuoha6@gmail.com>
2024-03-05 13:18:31 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A python function that queries the Reddit API and returns
-the number of subscribers 
-(not active users, total subscribers) 
+the number of subscribers
+(not active users, total subscribers)
 for a given subreddit """
 import requests
 
