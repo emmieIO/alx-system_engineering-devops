339b7be5687c16be9327bd2e75b5b582b64e6a9c
emmieIO <markonuoha6@gmail.com>
2024-03-05 13:17:03 +0100

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A python function that queries the Reddit API and returns
+the number of subscribers 
+(not active users, total subscribers) 
+for a given subreddit """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "Python Reddit API Script by /user/emmieio"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print("Error:", e)
+        return 0
